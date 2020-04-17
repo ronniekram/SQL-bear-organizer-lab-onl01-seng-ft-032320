b85a120fee8b597dec3a20b7bb99bcf62d51bbea
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-17 20:13:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,12 +21,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  binding.pry
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY 'value_occurrence' DESC LIMIT 1;"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT *, COUNT(temperament) FROM bears DESC LIMIT 1;"
+  "SELECT temperament, COUNT(temperament) FROM bears DESC LIMIT 1;"
 end
 
 def selects_bear_that_killed_Tim
