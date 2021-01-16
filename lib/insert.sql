15e46772bd3fa42d5c4b291418e7ed572e5218c1
Arynn Boniface <arynn.boniface@gmail.com>
2021-01-16 12:34:52 -0500

    commit tree eb9f80c0ff25565dc38ae7b2cf027e29e7358880

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'female', 'black', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 1, 'male', 'brown', 'agressive', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'female','black and white', 'timid', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'female', 'white', 'agressive', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 12, 'female', 'brown', 'docile', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'black', NULL, 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', NULL, 'female', 'brown', NULL, 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, 'male', 'black', 'agressive', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'female', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'other', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'prefer not to say', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'male', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, 'female', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8, 'male', 'brown', 'sweetie pie', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, 'male', 'brown', 'sweetie pie', true);
